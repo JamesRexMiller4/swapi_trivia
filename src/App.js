d8b1ced115ce38b387fbbfaa9e038c6ef4bbc464
Cameron MacRae <cam.mac.cm@gmail.com>
2019-12-03 16:31:06 -0700

    Add basic styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Landing from './components/Landing.js';
 
 function App() {
   return (
     <div className="App">
+      <Landing />
     </div>
   );
 }
---
+++ src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Landing.css';
+// import Form from './Form.js';
+
+const Landing = () => {
+  return (
+    <div id="landing-main">
+      <h1>Swapi</h1>
+    </div>
+  );
+}
+
+
+export default Landing;
