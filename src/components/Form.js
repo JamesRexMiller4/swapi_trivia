89ca50bbacd9230cedc2f4c3da37b93be4074c3c
Cameron MacRae <cam.mac.cm@gmail.com>
2019-12-03 18:23:12 -0700

    Finish landing pg styles

--- src/components/Form.js
+++ src/components/Form.js
@@ -12,12 +12,34 @@ class Form extends Component {
   }
   render() {
     return (
-      <div>
+      <div id="form-wrapper">
         <span>Please sign in to continue.</span>
-        <label>Name:</label>
-        <input type="text" id="name-input"></input>
-        <label>Favorite Quote:</label>
-        <input type="text" id="quote-input"></input>
+        <div className="input-wrapper">
+          <label>Name:</label>
+          <input type="text" id="name-input"></input>
+        </div>
+        <div className="input-wrapper">
+          <label>Favorite Quote:</label>
+          <input type="text" id="quote-input"></input>
+        </div>
+        <div className="input-wrapper">
+          <label id="button-main-label">Knowledge Ranking:</label>
+          <div id="radio-wrapper">
+            <div className="radio-wrapper-sm">
+              <input className="radio-input" type="radio" name="Novice" id="novice-input"></input>
+              <label className="radio-label" for="novice-input">Novice</label>
+            </div>
+            <div className="radio-wrapper-sm">
+              <input className="radio-input" type="radio" name="Novice" id="intermediate-input"></input>
+              <label className="radio-label" for="intermediate-input">Intermediate</label>
+            </div>
+            <div className="radio-wrapper-sm">
+              <input className="radio-input" type="radio" name="Novice" id="expert-input"></input>
+              <label className="radio-label" for="expert-input">Expert</label>
+            </div>
+          </div>
+        </div>
+        <button id="form-submit">Sign In</button>
       </div>
     )
   }
