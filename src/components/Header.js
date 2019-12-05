c5570633440757e1d5d0deaf8fbb0443e19c044d
James Miller <james.rex.miller.4@gmail.com>
2019-12-05 00:09:23 -0700

    Add elements and pass through props

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,15 @@ import './Header.scss';
 const Header = ({name, quote, ranking}) => {
   return (
     <header className="header">
-      <h2></h2>
-
+      <div className="logo-btn-wrapper">
+      <h1 className="logo-h1-header">Swapi WarS</h1>
+      <button className="logout-button-header" id="logout-btn">Log Out</button>
+      </div>
+      <div className="profile-wrapper-header">
+        <h2>Welcome {name}!</h2>
+        <h3 className="quote-h3-header">"{quote}"</h3>
+        <h3 className="ranking-h3-header">Ranking: <span>{ranking}</span></h3>
+      </div>
     </header>
   )
 }
