ce8d5f2674deb4f3097689d2214162b1791035b3
James Miller <james.rex.miller.4@gmail.com>
2019-12-04 18:07:47 -0700

    Add conditional render for loader component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Landing from './components/Landing.js';
+import Loader from './components/Loader.js'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      
+     
     }
   }
   render() {
     return (
       <div className="App">
         <Landing />
+        {!this.state && <Loader />}
       </div>
     );
   }
