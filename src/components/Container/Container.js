b493b4989643a1836aff2795987cf1312b56d23b
James Miller <james.rex.miller.4@gmail.com>
2019-12-05 00:54:02 -0700

    Add folders for all components with components directory

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Card from './Card'
 import './Container.scss';
 
 const Container = () => {
   return (
     <section className="container-section">
+      <Card />
     </section>
   )
 }
