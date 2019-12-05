4466227d8326872dd933c39ba50d693ed66d7d0c
James Miller <james.rex.miller.4@gmail.com>
2019-12-04 18:04:11 -0700

    Create Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Loader.css';
+
+
+const Loader = () =>  {
+  return (
+    <section className='loader-section'>
+      <h2 className='loader-h2'>A long time ago in a galaxy far far away
+      <div className="loader-div-wave">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </div></h2>
+    </section>
+  )
+}
+
+export default Loader;
