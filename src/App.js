29d0cb2b866d03803d461a305c495d4404d51b21
James Miller <james.rex.miller.4@gmail.com>
2019-12-05 22:36:02 -0700

    Add componentDidMount fetch logic

--- src/App.js
+++ src/App.js
@@ -19,22 +19,47 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    this.state.user && 
+    fetch('https://swapi.co/api/films/')
+      .then(res => res.json())
+      .then(data => {
+        let movies = data.results.map((movie, index) => {
+          let arr = movie.release_date.split('-')
+          let date = arr[0]
+            return ({
+              key: index,
+              id: movie.episode_id,
+              title: movie.title,
+              date: date,
+              characters: movie.characters
+            })
+          })
+        return movies
+      })
+      .then(movies => this.setState({movies}))
+      .catch(error => console.log(error))
+  }
+  
+
   updateLogin = ({ name, quote, ranking }) => {
     this.setState({ user: { name, quote, ranking }});
   }
+
 
   render = () => {
     return (
       <div className="App">
         <Route exact path='/' render={() => !this.state ? 
-        <Loader /> : <Landing updateLogin={this.updateLogin}/>} />
+        <Loader /> : <Landing updateLogin={this.updateLogin} />} />
         <Route path='/movies' render={() => 
         <Header 
           name={this.state.user.name} 
           quote={this.state.user.quote}
           ranking={this.state.user.ranking}/>} 
         />
-        <Route exact path='/movies' render={() => <Container />} />
+        <Route exact path='/movies' render={() => !this.state.movies ? 
+        <Loader /> : <Container movies={this.state.movies} />} />
         <Route path='/movies/:movie_id' render={() => <Container />} />
         <Route path='/favorite' render={() => <Container />} />
       </div>
