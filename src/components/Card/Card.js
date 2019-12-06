3bf9961283d3361dfbe2b149aab021dd74d7d64c
Cameron MacRae <cam.mac.cm@gmail.com>
2019-12-05 22:38:08 -0700

    Fix card styling

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Card.scss';
 
 const Card = () => {
-// For movie cards the props being passed down will be 
+// For movie cards the props being passed down will be
 
 
 // For character cards the props being spread out and passed down will need to be grabbed
 // as {id, name, homeworld, species}
 
   // homeworld, population of homeworld is a url for another fetch request
-  // species is another fetch request 
+  // species is another fetch request
   // films is another fetch request
 
   return (
@@ -17,81 +17,81 @@ const Card = () => {
     <section className="section-card">
       <div className="wrapper-card-div">
         <ul className="card-details-ul">
-          <li>Name: Luke SkyWalker</li>
-          <li>Homeworld: Tattoiine</li>
-          <li>Population of Tatootine: 400 million</li>
-          <li>Species: Human</li>
-          <li>Other Films: all seven</li>
-        </ul>
-      </div>
-    </section>
-    <section className="section-card">
-      <div className="wrapper-card-div">
-        <ul className="card-details-ul">
-          <li>Name: Luke SkyWalker</li>
-          <li>Homeworld: Tattoiine</li>
-          <li>Population of Tatootine: 400 million</li>
-          <li>Species: Human</li>
-          <li>Other Films: all seven</li>
+          <li><strong>Name:</strong> Luke SkyWalker</li>
+          <li><strong>Homeworld:</strong> Tattoiine</li>
+          <li><strong>Population of Tatootine:</strong> 400 million</li>
+          <li><strong>Species:</strong> Human</li>
+          <li><strong>Other Films:</strong> all seven</li>
         </ul>
       </div>
     </section>
     <section className="section-card">
     <div className="wrapper-card-div">
       <ul className="card-details-ul">
-        <li>Name: Luke SkyWalker</li>
-        <li>Homeworld: Tattoiine</li>
-        <li>Population of Tatootine: 400 million</li>
-        <li>Species: Human</li>
-        <li>Other Films: all seven</li>
+        <li><strong>Name:</strong> Luke SkyWalker</li>
+        <li><strong>Homeworld:</strong> Tattoiine</li>
+        <li><strong>Population of Tatootine:</strong> 400 million</li>
+        <li><strong>Species:</strong> Human</li>
+        <li><strong>Other Films:</strong> all seven</li>
       </ul>
     </div>
-  </section>
-  <section className="section-card">
-  <div className="wrapper-card-div">
-    <ul className="card-details-ul">
-      <li>Name: Luke SkyWalker</li>
-      <li>Homeworld: Tattoiine</li>
-      <li>Population of Tatootine: 400 million</li>
-      <li>Species: Human</li>
-      <li>Other Films: all seven</li>
-    </ul>
-  </div>
-</section>
-<section className="section-card">
-      <div className="wrapper-card-div">
-        <ul className="card-details-ul">
-          <li>Name: Luke SkyWalker</li>
-          <li>Homeworld: Tattoiine</li>
-          <li>Population of Tatootine: 400 million</li>
-          <li>Species: Human</li>
-          <li>Other Films: all seven</li>
-        </ul>
-      </div>
     </section>
     <section className="section-card">
     <div className="wrapper-card-div">
       <ul className="card-details-ul">
-        <li>Name: Luke SkyWalker</li>
-        <li>Homeworld: Tattoiine</li>
-        <li>Population of Tatootine: 400 million</li>
-        <li>Species: Human</li>
-        <li>Other Films: all seven</li>
+        <li><strong>Name:</strong> Luke SkyWalker</li>
+        <li><strong>Homeworld:</strong> Tattoiine</li>
+        <li><strong>Population of Tatootine:</strong> 400 million</li>
+        <li><strong>Species:</strong> Human</li>
+        <li><strong>Other Films:</strong> all seven</li>
       </ul>
     </div>
-  </section>
-  <section className="section-card">
-  <div className="wrapper-card-div">
-    <ul className="card-details-ul">
-      <li>Name: Luke SkyWalker</li>
-      <li>Homeworld: Tattoiine</li>
-      <li>Population of Tatootine: 400 million</li>
-      <li>Species: Human</li>
-      <li>Other Films: all seven</li>
-    </ul>
+    </section>
+    <section className="section-card">
+    <div className="wrapper-card-div">
+      <ul className="card-details-ul">
+        <li><strong>Name:</strong> Luke SkyWalker</li>
+        <li><strong>Homeworld:</strong> Tattoiine</li>
+        <li><strong>Population of Tatootine:</strong> 400 million</li>
+        <li><strong>Species:</strong> Human</li>
+        <li><strong>Other Films:</strong> all seven</li>
+      </ul>
+    </div>
+    </section>
+    <section className="section-card">
+      <div className="wrapper-card-div">
+        <ul className="card-details-ul">
+          <li><strong>Name:</strong> Luke SkyWalker</li>
+          <li><strong>Homeworld:</strong> Tattoiine</li>
+          <li><strong>Population of Tatootine:</strong> 400 million</li>
+          <li><strong>Species:</strong> Human</li>
+          <li><strong>Other Films:</strong> all seven</li>
+        </ul>
+      </div>
+    </section>
+    <section className="section-card">
+      <div className="wrapper-card-div">
+        <ul className="card-details-ul">
+          <li><strong>Name:</strong> Luke SkyWalker</li>
+          <li><strong>Homeworld:</strong> Tattoiine</li>
+          <li><strong>Population of Tatootine:</strong> 400 million</li>
+          <li><strong>Species:</strong> Human</li>
+          <li><strong>Other Films:</strong> all seven</li>
+        </ul>
+      </div>
+    </section>
+    <section className="section-card">
+      <div className="wrapper-card-div">
+        <ul className="card-details-ul">
+          <li><strong>Name:</strong> Luke SkyWalker</li>
+          <li><strong>Homeworld:</strong> Tattoiine</li>
+          <li><strong>Population of Tatootine:</strong> 400 million</li>
+          <li><strong>Species:</strong> Human</li>
+          <li><strong>Other Films:</strong> all seven</li>
+        </ul>
+      </div>
+    </section>
   </div>
-</section>
-</div>
   )
 }
 
