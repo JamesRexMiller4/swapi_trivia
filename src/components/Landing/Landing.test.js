b5ea1321b9746a1a793610ceaff3dec5cc3c96cd
James Miller <james.rex.miller.4@gmail.com>
2019-12-07 10:32:29 -0700

    Add snapshot test to Landing

--- src/components/Landing/Landing.test.js
+++ src/components/Landing/Landing.test.js
@@ -4,11 +4,24 @@ import Landing from './Landing';
 import {shallow} from 'enzyme';
 
 describe('Landing', () => {
+  let wrapper, mockLoginUpdate;
+
+  beforeEach(() => {
+    mockLoginUpdate = jest.fn()
+
+    wrapper = shallow(<Landing 
+      mockLoginUpdate={mockLoginUpdate}
+    />)
+  })
 
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Landing />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should match the snapshot with all the data passed through', () => {
+    expect(wrapper).toMatchSnapshot()
   });
 
 });
