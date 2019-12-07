641d0ef36ced33494162673d42ef22be68e3f79d
James Miller <james.rex.miller.4@gmail.com>
2019-12-06 23:58:02 -0700

    Remove unused base css file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
 
 const router = (
@@ -9,6 +8,5 @@ const router = (
     <App />
   </BrowserRouter>
 )
-
 
 ReactDOM.render(router, document.getElementById('root'));
