a872e76347fd2fd8f6c66ffffac17f20d84620a4
James Miller <james.rex.miller.4@gmail.com>
2019-12-08 10:10:45 -0700

    Add passing snapshot test for container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -4,19 +4,27 @@ import Card from '../Card/Card.js'
 import './Container.scss';
 
 const Container = (props) => {
+  let cardData;
 
+  if (props.data.type !== 'movie') {
   const tenToDisplay = props.data.slice(0, 10)
-  const cardData = tenToDisplay.map((data, index) => {
+  cardData = tenToDisplay.map((data, index) => {
   return (<Card {...data} setPath={props.setPath} key={index}/>)
   })
 
-  return (
-    <section className="container-section">
-      <div className="grid-container">
-        {cardData}
-      </div>
-    </section>
-  )
+  } else {
+    cardData = props.data.map((data, index) => {
+      return (<Card {...data} setPath={props.setPath} key={index}/>)
+      })
+    }
+
+    return (
+      <section className="container-section">
+        <div className="grid-container">
+          {cardData}
+        </div>
+      </section>
+    )
 }
 
 Container.propTypes = {
--- src/components/Container/Container.test.js
+++ src/components/Container/Container.test.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import Container from './Container';
+import Card from '../Card/Card'
 import {shallow} from 'enzyme';
 
 describe('Container', () => {
-  let wrapper, mockData;
-
-    beforeEach(() => {
-      wrapper = shallow(<Container data={mockData} />)
-    })
+  let wrapper, data;
 
   it('should match the snapshot with all the movie data passed through', () => {
-    mockData = [
+    data = [
     {key: 4, id: 4, title: 'A NEW HOPE', date: '1977', characters: ['Luke', 'RD-D2', 'Chewie'],
     openingCrawl: 'In the beginning, there was Jack', type: 'movie'}, 
     {key: 5, id: 5, title: 'THE EMPIRE STRIKES BACK', date: '1980', characters: ['Han', 'Vadar', 'C3PO'],
@@ -20,6 +17,7 @@ describe('Container', () => {
     {key: 5, id: 7, title: 'THE FORCE AWAKENS', date: '2015', characters: ['B-B8', 'Chewi', 'Snope'],
     openingCrawl: 'And while one day viciously throwing down on his box, Jack boldly declared LET THERE BE HOUSE', type: 'movie'}
   ]
+    wrapper = shallow(<Container data= {data} />)
 
     expect(wrapper).toMatchSnapshot()
   })
