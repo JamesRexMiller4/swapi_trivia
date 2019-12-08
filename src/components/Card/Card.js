f583f08e82098d2d0b45e37bd3a8b575d5d649c0
James Miller <james.rex.miller.4@gmail.com>
2019-12-08 08:01:56 -0700

    Remove commmented out pseudo code + tidy up

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,25 +4,17 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const Card = (props) => {
-// For character cards the props being spread out and passed down will need to be grabbed
-// as {id, name, homeworld, species}
 
-  // homeworld, population of homeworld is a url for another fetch request
-  // species is another fetch request
-  // films is another fetch request
-  
   let movieList;
  if (props.type !== 'movie') {
-    let movies = props[2]
-    console.log(movies[0])
     movieList = props[2].map(movie => {
-      console.log(movie)
     return <li>{movie}</li>
       })
  }
    
     let card;
-    props.type === 'movie' ? card =
+    props.type === 'movie' ? 
+    card =
       <div className="wrapper-card-div">
         <ul className="card-details-ul">
           <li><strong className="episode-title">Episode {props.id}:</strong> {props.title}</li>
@@ -31,7 +23,8 @@ const Card = (props) => {
         <Link to={`/movies/${props.id}`} key={props.id}>
           <button className="character-btn" id={props.id} onClick={props.setPath}>View Characters</button>
         </Link>
-      </div> :
+      </div> 
+      :
     card =
       <div className="wrapper-card-div">
         <ul className="card-details-ul">
@@ -49,22 +42,6 @@ const Card = (props) => {
       </section>
     )
   }
-// }
-
-// For character cards
-// return (
-//   <section className="section-card">
-//   <div className="wrapper-card-div">
-//     <ul className="card-details-ul">
-//       <li><strong>Name:</strong> Luke SkyWalker</li>
-//       <li><strong>Homeworld:</strong> Tattoiine</li>
-//       <li><strong>Population of Tatootine:</strong> 400 million</li>
-//       <li><strong>Species:</strong> Human</li>
-//       <li><strong>Other Films:</strong> all seven</li>
-//     </ul>
-//   </div>
-// </section>
-// )
 
 Card.propTypes = {
   id: PropTypes.number,
